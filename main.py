552f27448fc3ff465afa449ffed4e5cbecc299e4
seren <3425742307@qq.com>
2026-03-09 17:12:09 +0800

    implement an userlogin API endpoint

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import bcrypt
 import database
 import pymysql.cursors
 
+# 用户注册
 class UserRegister(BaseModel):
+    username:str
+    password:str
+
+# 用户登录
+class UserLogin(BaseModel):
     username:str
     password:str
 
 app = FastAPI()
 
+# 用户注册API
 @app.post("/register/")
 async def register(userregister:UserRegister):
     pw=userregister.password.encode()
@@ -22,3 +29,17 @@ async def register(userregister:UserRegister):
             cursor.execute(sql,(userregister.username,hashed.decode("utf-8")))
         connection.commit()
     return userregister.username+' '+"register successfully!"
+
+# 用户登录API
+@app.post("/login/")
+async def login(userlogin:UserLogin):
+    connection = database.get_connection()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT password from users where users.username=%s"
+            cursor.execute(sql,(userlogin.username,))
+            result = cursor.fetchone()
+    if bcrypt.checkpw(userlogin.password.encode(),result[0].encode()):
+        return "login successfully"
+    else :
+        return "failed to login"
